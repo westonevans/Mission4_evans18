54e44457d75dbd188bf1109524a7ea17e475a7a4
weston <westonevans14@gmail.com>
2023-02-06 16:03:44 -0700

    commit change

---
+++ Mission4_evans18/Controllers/BlahController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission5_evans18.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calc()
+        {
+            return View();
+        }
+    }
+}
